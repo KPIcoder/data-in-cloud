2d038ef7ebf6e2feb8cbc9168565927370679d05
kpicoder <vladshcheglov41@gmail.com>
2024-05-24 15:47:04 +0100

    pre final tweaks

--- DataInCloud.Api/Program.cs
+++ DataInCloud.Api/Program.cs
@@ -24,6 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
