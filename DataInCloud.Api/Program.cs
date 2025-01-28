a7dd74453744055657a62a0c30698bdb6929eeb4
kpicoder <vladshcheglov41@gmail.com>
2025-01-28 03:09:44 -0500

    allow all origins

--- DataInCloud.Api/Program.cs
+++ DataInCloud.Api/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigins", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("*")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
